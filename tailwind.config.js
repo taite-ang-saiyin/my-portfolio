0f93d97331a26fa04c155e66f5fc5e08e250f5ad
Htet Aung Shine <114334766+taite-ang-saiyin@users.noreply.github.com>
2024-12-11 23:38:59 +0630

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#d6b39d', // Add your custom color
+      },
+  },
+  plugins: [],
+}
+}
